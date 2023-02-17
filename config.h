14048085530534298a37a71a97719cd1c4bda210
Encore Linux <125548408+encorelinux@users.noreply.github.com>
2023-02-17 19:42:20 +0100

    Patch 1: barpadding
    - Added patch "barpadding"
    - set resizehints to 0 in config.def.h
    - set default terminal to alacritty
    
    ============================
    Patch Information:
    http://dwm.suckless.org/patches/barpadding/
    
    Description
    
    This patch adds variables for verticle and horizontal space between the statusbar and the edge of the screen; unlike statuspadding, which adds padding between the bar's content and the edge of the bar. This patch adds two new variables (both default to 10) to config.def.h:
    
        vertpad (amount of vertical padding)
        sidepad (amount of padding either side of the bar)
    
    Compatible with both top and bottom bars as well as the togglebar function.
    
    Please note the following:
    
        Modifies config.def.h, not config.h. You may need to add rm config.h to the 'clean' targets in the Makefile.
        Does not add padding between the statusbar and the window clients. This is so that if you apply the fullgaps patch or similar, there will be equal space between the windows/screen edge and windows/bar.
        May need resizehints to be set to 0 to make the patch look proper, especially if your bar is on the bottom. Modify the 'resizehints' variable in config.def.h.

--- config.h
+++ config.h
@@ -5,6 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar  ---PATCH: barpadding--- */
+static const int sidepad            = 10;       /* horizontal padding of bar ---PATCH: barpadding--- */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -34,7 +36,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -58,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */

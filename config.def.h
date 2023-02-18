56a12a405d72d4c25ff6055a81b7cf1b1b14279e
Encore Linux <125548408+encorelinux@users.noreply.github.com>
2023-02-18 12:57:36 +0100

    Patch 7: movestack
    - Added patch "movestack"
    - cosmetic changes in comments in config.def.h
    =======================================
    Additional Information:
    
    movestack
    Description
    
    This plugin allows you to move clients around in the stack and swap them with the master. It emulates the behavior off mod+shift+j and mod+shift+k in Xmonad. movestack(+1) will swap the client with the current focus with the next client. movestack(-1) will swap the client with the current focus with the previous client.
    Usage
    
        Download the patch and apply according to the general instructions.
    
        Include the movestack.c source file and add keys that call movestack. Example from config.default.h:
    
         #include "movestack.c"
         static Key keys[] = {
         /* modifier                     key        function        argument */
         ...
         { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
         { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar  ---PATCH: barpadding--- */
-static const int sidepad            = 10;       /* horizontal padding of bar ---PATCH: barpadding--- */
+static const int vertpad            = 10;       /* vertical padding of bar  --- PATCH: barpadding--- */
+static const int sidepad            = 10;       /* horizontal padding of bar --- PATCH: barpadding--- */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -39,20 +39,20 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#include "fibonacci.c"
-#include "gaplessgrid.c"
-#include "horizgrid.c"
+#include "fibonacci.c"					/* --- PATCH: fibonacci */
+#include "gaplessgrid.c"				/* --- PATCH: gaplessgrid */
+#include "horizgrid.c"					/* --- PATCH: horizgrid */
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },    			/* first entry is default */
+	{ "><>",      NULL },    			/* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "[@]",      spiral },  /* ---PATCH: fibonacci --- */
- 	{ "[\\]",      dwindle },	/* ---PATCH: fibonacci --- */
-	{ "TTT",      bstack },  /* ---PATCH: bottomstack --- */
-	{ "===",      bstackhoriz }, /* ---PATCH: bottomstack --- */
- 	{ ":::",      gaplessgrid },  /* --- PATCH: gaplessgrid --- */
-	{ "---",      horizgrid },   /* --- PATCH: horizgrid --- */
+	{ "[@]",      spiral },  			/* --- PATCH: fibonacci --- */
+ 	{ "[\\]",      dwindle },			/* --- PATCH: fibonacci --- */
+	{ "TTT",      bstack },  			/* --- PATCH: bottomstack --- */
+	{ "===",      bstackhoriz }, 		/* --- PATCH: bottomstack --- */
+ 	{ ":::",      gaplessgrid },  		/* --- PATCH: gaplessgrid --- */
+	{ "---",      horizgrid },   		/* --- PATCH: horizgrid --- */
 };
 
 /* key definitions */
@@ -71,6 +71,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+#include "movestack.c"					/* --- PATCH: movestack */
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	/* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
@@ -83,9 +85,11 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, /* -- Changes size of vertical tiles */
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, /* -- Changes size of vertical tiles */
-	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} }, /* ---PATCH: cfacts */
-	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} }, /* ---PATCH: cfacts */
-	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} }, /* ---PATCH: cfacts */
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },   /* --- PATCH: movestack */
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },   /* --- PATCH: movestack */
+	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} }, /* --- PATCH: cfacts */
+	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} }, /* --- PATCH: cfacts */
+	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} }, /* --- PATCH: cfacts */
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
